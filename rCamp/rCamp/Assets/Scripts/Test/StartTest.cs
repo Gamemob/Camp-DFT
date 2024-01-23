25f2e31b48e277b8668bd8215f7379967b742392
chuuyamywife <138189317+chuuyamywife@users.noreply.github.com>
2024-01-23 17:54:43 +0800

    Third

--- rCamp/rCamp/Assets/Scripts/Test/StartTest.cs
+++ rCamp/rCamp/Assets/Scripts/Test/StartTest.cs
@@ -7,11 +7,13 @@ public class StartTest : MonoBehaviour
     public GameObject strattest;
     public GameObject test;
     public GameObject book;
+    public GameObject submit;
     public void Yes()
     {
         book.active = false;
         test.active = true;
         strattest.active = false;
+        submit.SetActive(true);
     }
     public void No()
     {
--- rCamp/rCamp/Assets/Scripts/Test/TestCheck.cs
+++ rCamp/rCamp/Assets/Scripts/Test/TestCheck.cs
@@ -6,7 +6,10 @@ public class TestCheck : MonoBehaviour
 {
     public static int wrongNumer = 0;
     public GameObject passedText;
+    public GameObject passedButton;
     public GameObject wrongText;
+    public GameObject backButton;
+    public GameObject replayButton;
     public GameObject testPage;
     public GameObject cluePage;
 
@@ -15,10 +18,13 @@ public class TestCheck : MonoBehaviour
         if(wrongNumer == 0)
         {
             passedText.active = true;
+            passedButton.SetActive(true);
         }
         else
         {
             wrongText.active = true;
+            backButton.SetActive(true);
+            replayButton.SetActive(true);
         }
         testPage.active = false;
         cluePage.active = false;
